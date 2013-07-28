c76b0e7975352e866f656bcdd77c390424aeb61d
Blake Dietz <blakedietz@gmail.com>
2013-07-27 18:33:43 -0600

    Improved sorting of charts.  Removed tick box for sorting. Separated data in pyramidChart.js into data.js for modularity.

---
+++ data.js
@@ -0,0 +1,202 @@
+var data = [
+	{
+		"state":"Alaska",
+		"percentage": -3.2
+	},
+	{
+		"state":"Alabama",
+		"percentage": -39.8
+	},
+	{
+		"state":"Arizona",
+		"percentage": -50.4
+	},
+	{
+		"state":"Arkansas",
+		"percentage":-18.9
+	},
+	{
+		"state":"California",
+		"percentage":-29.3
+	},
+	{
+		"state":"Colorado",
+		"percentage":-32.3
+	},
+	{
+		"state":"Connecticut",
+		"percentage":-23.1
+	},
+	{
+		"state":"Delaware",
+		"percentage":-25
+	},
+	{
+		"state":"Florida",
+		"percentage": -41.2
+	},
+	{
+		"state":"Georgia",
+		"percentage":-29.5
+	},
+	{
+		"state":"Hawaii",
+		"percentage":-25.3
+	},
+	{
+		"state":"Idaho",
+		"percentage": -39.6
+	},
+	{
+		"state":"Illinois",
+		"percentage":-23.3
+	},
+	{
+		"state":"Indiana",
+		"percentage":-17.2
+	},
+	{
+		"state":"Iowa",
+		"percentage":-27.7
+	},
+	{
+		"state":"Kansas",
+		"percentage":-24.5
+	},
+	{
+		"state":"Kentucky",
+		"percentage":-26.3
+	},
+	{
+		"state":"Louisiana",
+		"percentage": -42
+	},
+	{
+		"state":"Maine",
+		"percentage":-15.7
+	},
+	{
+		"state":"Maryland",
+		"percentage":-18.3
+	},
+	{
+		"state":"Massachusetts",
+		"percentage": -37.4
+	},
+	{
+		"state":"Michigan",
+		"percentage": -32.4
+	},
+	{
+		"state":"Minnesota",
+		"percentage":-30.4
+	},
+	{
+		"state":"Mississippi",
+		"percentage":-32.4
+	},
+	{
+		"state":"Missouri",
+		"percentage":-29.7
+	},
+	{
+		"state":"Montana",
+		"percentage":-16.6
+	},
+	{
+		"state":"Nebraska",
+		"percentage":-16.6
+	},
+	{
+		"state":"Nevada",
+		"percentage":-31.2
+	},
+	{
+		"state":"New Hampshire",
+		"percentage": -49.9
+	},
+	{
+		"state":"New Jersey",
+		"percentage": -27.2
+	},
+	{
+		"state":"New Mexico",
+		"percentage": -36.7
+	},
+	{
+		"state":"New York",
+		"percentage": -14.7
+	},
+	{
+		"state":"North Carolina",
+		"percentage": -14.6
+	},
+	{
+		"state":"North Dakota",
+		"percentage": 16.5
+	},
+	{
+		"state":"Ohio",
+		"percentage":-28.9
+	},
+	{
+		"state":"Oklahoma",
+		"percentage": -26.2
+	},
+	{
+		"state":"Oregon ",
+		"percentage": -43.6
+	},
+	{
+		"state":"Pennsylvania",
+		"percentage":-29.9
+	},
+	{
+		"state":"Rhode Island",
+		"percentage":-24.8
+	},
+	{
+		"state":"South Carolina",
+		"percentage": -38.8
+	},
+	{
+		"state":"South Dakota",
+		"percentage": -22.1
+	},
+	{
+		"state":"Tennessee",
+		"percentage":-30.1
+	},
+	{
+		"state":"Texas",
+		"percentage":-22.7
+	},
+	{
+		"state":"Utah",
+		"percentage":-30.6
+	},
+	{
+		"state":"Vermont",
+		"percentage":-18.6
+	},
+	{
+		"state":"Virginia",
+		"percentage":-27.8
+	},
+	{
+		"state":"Washington",
+		"percentage": -37.5
+	},
+	{
+		"state":"West Virginia",
+		"percentage": -17.8
+	},
+	{
+		"state":"Wisconsin",
+		"percentage":-17.5
+	},
+	{
+		"state":"Wyoming",
+		"percentage":7
+	},
+];
